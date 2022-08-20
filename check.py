39c286989c6af6059b418d9ad49ea6b3607b53cf
Odysseyau <42457390+Odysseyau@users.noreply.github.com>
2022-08-20 23:22:25 +0800

    Update check.py

--- check.py
+++ check.py
@@ -127,6 +127,7 @@ def check(cookies, geo_api_info, id, uid):
         'sfzx': '1',  #是否在校1是 0否。
         'gzczxq':'2', #工作常驻校区：1明故宫 2将军路 3天目湖
         'sfzhux': '1', #是否住校 1是 0否
+        'sfzgn': '1',
         #'zhuxdz': '',
         'njrddz': "南区35栋",
         'szgj': '',
